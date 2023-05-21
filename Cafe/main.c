00a4aa96dfc72792d69dada17726cc23859812a2
ratul41907 <133839523+ratul41907@users.noreply.github.com>
2023-05-21 18:00:33 +0600

    Update 1
    Added loops and structures

--- Cafe/main.c
+++ Cafe/main.c
@@ -5,10 +5,11 @@
 #include<dos.h>
 int main()
 {
-    char name[10],pass[10];
+
     screen();
 
     welcome();
+    menu();
     getch();
 }
 void screen() //splash screen
@@ -61,14 +62,23 @@ void welcome(int uid,int pass,int i) //welcome screen
     printf("\t\t\t               |______________________________________________|");
 
 
-    printf("                                           \t\t\t\t\t\t User ID : ");
+    printf("\n\n                                           \t\t User ID : ");
     scanf("%d",&uid);
     printf("\n");
 
     printf("                     \t\t\t\t\t Password : ");
     scanf(" %d",&pass);
 
-      while(uid!=231539 && pass!=123)
+      if(uid==2311539 && pass==123)
+    {
+        printf("\n\n                                                         Logged in Successfully\n");
+         sleep(1);
+        system("cls");
+        menu();
+    }
+     else
+     {
+        while(uid!=231539 && pass!=123)
       {
     printf("\t\t\t\t\t\tPlease Enter A Proper User Name or Password\n");
     sleep(2);
@@ -86,10 +96,47 @@ void welcome(int uid,int pass,int i) //welcome screen
     scanf(" %d",&pass);
 
       }
+     }
+}
+void menu()
+{
+      system("cls"); //used system cls as it showed output twice,deleted 1st output
+
+    printf("\t\t\t                ______________________________________________\n\n");
+    printf("\t\t\t               |                 Nac Cafe                     |\n ");
+    printf("\t\t\t               |______________________________________________|");
+
+     struct list
+     {
+       int sl;
+       char item[20];
+       float sprice;
+       float mprice;
+       float lprice;
+     };
+     struct list a1;
 
 
 
 
 
+     a1.sl=01;
+     strcpy(a1.item,"Mocha");
+     a1.sprice=210.0;
+     a1.mprice=330.0;
+     a1.lprice=380.0;
+     printf("\n\n               ----------------------------------------------------------------------------------------------\n");
+
+
+     printf("               | Sl NO |                      Item Name                            | Small | Medium | Large | \n     ");
+     printf("          ----------------------------------------------------------------------------------------------\n");
+     printf("               |   %d   |   %s                                                   | %.2f|  %.2f| %.2f|\n",a1.sl,a1.item,a1.sprice,a1.mprice,a1.lprice);
 
 }
+
+
+
+
+
+
+
