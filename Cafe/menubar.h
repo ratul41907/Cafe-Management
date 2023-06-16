b2eb76658142b0c6e1dd3c77d031176fb5cfd7bd
ratul41907 <133839523+ratul41907@users.noreply.github.com>
2023-06-16 15:59:10 +0600

    Update 41

--- Cafe/menubar.h
+++ Cafe/menubar.h
@@ -18,63 +18,31 @@ void menubar()
              //  while(n!=1 |\ n!=2 || n!=3)
 
 
-               if(n==1)
-               {
-                  system("cls");
-                   menu();
+              switch (n)
+              {
 
+        case 1:
+            system("cls");
+            menu();
+            break;
 
-               }
-               else if(n==2)
-               {
-                   system("cls");
-                   bill();
-               }
-               else if(n==3)
-               {
-                   exit(1);
-               }
+        case 2:
+            system("cls");
+            bill();
+            break;
 
-               else
-                {
+        case 3:
+            exit(1);
+            break;
 
-                     while(n!=1 || n!=2 || n!=3)
-                     {
-       printf("\n\n                            Plesae Enter Valid Number\n");
+        default:
+            printf("\n\n                            Please Enter a Valid Number\n");
+            Sleep(1800);
+            system("cls");
+            menubar();
+            break;
 
-                       system("cls");
-       printf("\t\t\t                ______________________________________________\n\n");
-       printf("\t\t\t               |                 Nac Cafe                     |\n ");
-       printf("\t\t\t               |______________________________________________|");
-       printf("\n\n                                                  Enter A valid Number\n\n");
-     printf("\n\n\n\n                 Please Enter A Number Given Below\n");
-
-        printf("\n                       1.Menu");
-        printf("\n                       2.Calculate Bill");
-        printf("\n                       3.Exit");
-        printf("\n                    Enter Here : ");
-            scanf("%d",&n);
-
-
-             if(n==1)
-               {
-                  system("cls");
-                   menu();
-
-
-               }
-               else if(n==2)
-               {
-                   system("cls");
-                   bill();
-               }
-               else if(n==3)
-               {
-                   exit(1);
-               }
-                }
-                }
-
+    }
 
 
 }
